315eed06091b32f467b2536f9323bd87ad9c0216
D DARSHAN <darshand4802@gmail.com>
2025-07-14 20:40:29 +0530

    Initial commit

---
+++ src/gemini.js
@@ -0,0 +1,40 @@
+import { prevUser } from "./context/UserContext";
+
+const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyCE8rMtvQ94nRw36T6qqxlKTZIHd6ucP_Q"
+// AIzaSyCE8rMtvQ94nRw36T6qqxlKTZIHd6ucP_Q
+
+export async function generateResponse() {
+
+    let RequestOption={
+        method:"POST",
+        Headers: {'Content-Type' : 'application/json'},
+        body:JSON.stringify({
+            "contents": [
+    {
+      "parts": [
+        {"text": prevUser.prompt,},
+        prevUser.data?[{
+          "inline_data": {
+            "mime_type": prevUser.mime_type,
+            "data": prevUser.data,
+          }
+        }] : []
+        
+      ]
+    }
+  ]
+        })
+    }
+    try{
+        let response=await fetch(Api_Url, RequestOption)
+        let data = await response.json()
+        let apiResponse = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim()
+        return apiResponse;
+    }
+    catch{
+
+    }
+}
+
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,164 @@
+import React, { useContext } from "react";
+import "../App.css";
+import { RiImageAiFill } from "react-icons/ri";
+import { RiImageAddLine } from "react-icons/ri";
+import { MdChatBubbleOutline } from "react-icons/md";
+import { FiPlus } from "react-icons/fi";
+import { FaArrowUpLong } from "react-icons/fa6";
+import { dataContext, prevUser, user } from "../context/UserContext";
+import Chat from "./Chat";
+import { generateResponse } from "../gemini";
+
+const Home = () => {
+  let {
+    startRes,
+    setStartRes,
+    popUp,
+    setPopUP,
+    input,
+    setInput,
+    feature,
+    setFeature,
+    showResult,
+    setShowResult,
+    prevFeature, setPrevFeature,
+  } = useContext(dataContext);
+
+  async function handleSubmit(e) {
+
+    setStartRes(true);
+    setPrevFeature(feature)
+
+    setShowResult("");
+    prevUser.data = user.data;
+    prevUser.mime_type = user.mime_type;
+    prevUser.imgUrl = user.imgUrl;
+    prevUser.prompt = input;
+    setInput("");
+    let result = await generateResponse();
+    setShowResult(result);
+    setFeature("chat")
+    user.data = null;
+    user.mime_type = null;
+    user.imgUrl = null;
+  }
+
+  // ---------Handling the Image Fetched from Browsing-------------
+  function handleImage(e) {
+    setFeature("upimg");
+    let file = e.target.files[0];
+
+    let reader = new FileReader();
+    reader.onload = (event) => {
+      let base64 = event.target.result.split(",")[1];
+      user.data = base64;
+      user.mime_type = file.type;
+      user.imgUrl = `data:${user.mime_type};base64,${user.data}`;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <div className="home">
+      {/* Navbar Section */}
+      <nav>
+        <div className="logo" onClick={()=>{
+          setStartRes(false)
+          setFeature("chat")
+        }}>Smart AI Bot</div>
+      </nav>
+
+      {/* ---------------Browse Images------------------ */}
+      <input
+        type="file"
+        accept="image/*"
+        hidden
+        id="inputImg"
+        onChange={handleImage}
+      />
+
+      {!startRes ? (
+        <div className="hero">
+          <span id="tag">What can I hep with?</span>
+          <div className="cate">
+            <div
+              className="upImg"
+              onClick={() => {
+                document.getElementById("inputImg").click();
+              }}
+            >
+              <RiImageAddLine />
+              <span>Upload Image</span>
+            </div>
+            <div className="genImg" onClick={() => setFeature("genimg")}>
+              <RiImageAiFill />
+              <span>Generate Image</span>
+            </div>
+            <div className="chat" onClick={() => setFeature("chat")}>
+              <MdChatBubbleOutline />
+              <span>Let's Chat</span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Chat />
+      )}
+
+      {/* Hero-Section */}
+
+      <form
+        className="input-box"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (input) {
+            handleSubmit(e);
+          }
+        }}
+      >
+        {popUp ? (
+          <div className="pop-up">
+            <div
+              className="select-up"
+              onClick={() => {
+                setPopUP(false);
+                setFeature("chat")
+                document.getElementById("inputImg").click();
+              }}
+            >
+              <RiImageAddLine />
+              <span>Upload Image</span>
+            </div>
+            <div className="select-gen" onClick={() => {
+              setPopUP(false);
+              setFeature("genImg")}}>
+              <RiImageAiFill />
+              <span>Generate Image</span>
+            </div>
+          </div>
+        ) : null}
+
+        <div
+          id="add"
+          onClick={() => {
+            setPopUP((prev) => !prev);
+          }}
+        >
+          {feature == "genimg" ? <RiImageAiFill id="genImg" /> : <FiPlus />}
+        </div>
+        <input
+          type="text"
+          placeholder="Ask Something..."
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        {input ? (
+          <button id="submit">
+            <FaArrowUpLong />
+          </button>
+        ) : null}
+      </form>
+    </div>
+  );
+};
+
+export default Home;
